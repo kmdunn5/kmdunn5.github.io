a67de497d5ee9a7001e364c5015c97a743b63624
kmdunn5 <kmdunn5@gmail.com>
2021-02-06 15:27:50 -0500

    added some game constants so I can call them later

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ console.log('Jack Black time!')
 //=====================//
 const gameStates = ['Player\'s Turn', 'Dealer\'s Turn', 'Game End'];
 let currentGameState = '';
+let playerTotal = '';
+let dealerTotal = '';
 
 
 //=====================//
@@ -13,6 +15,11 @@ let currentGameState = '';
 
 class BlackJackConstants {
     static $card = $('.card');
+    static $playerArea = $('#playerArea');
+    static $dealerArea = $('#dealerArea');
+    static $dealButton = $('#deal');
+    static $hitButton = $('#hit');
+    static $standButton = $('#stand');
     static cardNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
     static cardSuits = ['Heart', 'Diamond', 'Spade', 'Club'];
 }
@@ -26,10 +33,10 @@ class CreateCard {
         return Math.floor(Math.random() * key.length);
     }
     static cardNumber = () => {
-        return cardNumbers[CreateCard.randomCard(cardNumbers)];
+        return BlackJackConstants.cardNumbers[CreateCard.randomCard(BlackJackConstants.cardNumbers)];
     }
     static cardSuit = () => {
-        return cardSuits[CreateCard.randomCard(cardSuits)];
+        return BlackJackConstants.cardSuits[CreateCard.randomCard(BlackJackConstants.cardSuits)];
     };
 }
 
@@ -39,31 +46,45 @@ class Card {
         this.suit = suit
     }
 }
+
 //=====================//
 //====Button Methods===//
 //=====================//
 
 class ButtonsMethods {
-    deal = () => {
+    static deal = () => {
         //remove card divs
         //add card div to player
+        const $newCard = $('<div>').addClass('card');
+        $playerArea.append($newCard);
         //add card div to dealer facedown
         //add card div to player
         //add card div to deal
     }
-    hit = () => {
+    static hit = () => {
         // add card to player area
     }
-    stand = () => {
-        // set game state to xxxx
+    static stand = () => {
+        // set game state to dealer turn
     }
 }
 
+//=====================//
+//====Dealer's Turn====//
+//=====================//
 
+const dealerTurn = () => {
+    
+}
+
+
+//=====================//
+//==Button On Clicks===//
+//=====================//
 
 
 const card1 = new Card(CreateCard.cardNumber(), CreateCard.cardSuit());
 
-console.log(card1)
-console.log
+console.log(card1.number + ' of ' + card1.suit)
+console.log(BlackJackConstants.cardSuits)
 
