833b6c6338ed9dfa962ec050914be6b72d7ce6aa
kmdunn5 <kmdunn5@gmail.com>
2021-05-25 14:22:01 -0400

    worked on moving functions around to proper classes, will change logic soon

--- Black-Jack-Black/js/app2.js
+++ Black-Jack-Black/js/app2.js
@@ -45,22 +45,197 @@ class Card {
         this.suit = suit;
         this.face = face;
     }
+    randomCard = (key) => {
+        return Math.floor(Math.random() * key.length);
+    }
+    cardNumber = () => {
+        return BlackJackConstants.cardNumbers[this.randomCard(BlackJackConstants.cardNumbers)];
+    }
+    cardSuit = () => {
+        return BlackJackConstants.cardSuits[this.randomCard(BlackJackConstants.cardSuits)];
+    }
+    createCard = (number, suit, faceStatus) => {
+        let card = new Card(number, suit, faceStatus);
+        return card;
+    }
 }
 
 class Hand {
+    sum = (hand) => {
+        let total = 0;
+        let numberOfAces = 0;
+        for (let i = 0; i < hand.length; i++) {
 
+            if (hand[i].number === 'J' || hand[i].number === 'Q' || hand[i].number === 'K') {
+                total = total + 10;
+
+            } else if (hand[i].number === 'A') {
+                numberOfAces += 1
+
+            } else {
+                total = total + parseInt(hand[i].number, 10);
+            }
+        }
+        if (numberOfAces > 0) {
+            total += numberOfAces - 1;
+            if (total < 11) {
+                total += 11;
+            } else {
+                total += 1;
+            }
+        }
+        return total
+    }
 }
 
 class Deck {
+    dealCard = (area, face, hand) => {
+        let $newDiv = $('<div>').addClass('card');
+        let num = this.cardNumber();
+        let suit = this.cardSuit();
+        let newCard = this.createCard(num, suit, face);
+        hand.push(newCard);
+        $newDiv.addClass(`${num} ${suit} ${newCard.face}`);
+        area.append($newDiv);
+    }
 
+    deal = () => {
+        if (currentGameState === gameStates[2] || currentGameState === '') {
+            
+            //========remove card divs and reset game
+            
+            winner = '';
+            $('.card').remove();
+            BlackJackConstants.playerHand.splice(0, BlackJackConstants.playerHand.length);
+            BlackJackConstants.dealerHand.splice(0, BlackJackConstants.dealerHand.length);
+            playerTotal = BlackJackMethods.sum(BlackJackConstants.playerHand);
+            $('#playerTotal').text(playerTotal);
+            dealerTotal = BlackJackMethods.sum(BlackJackConstants.dealerHand);
+            $('#dealerTotal').text(dealerTotal);
+            
+            //=======add card div to player
+
+            CardMethods.dealCard(BlackJackConstants.$playerArea, 'up', BlackJackConstants.playerHand);
+            playerTotal = BlackJackMethods.sum(BlackJackConstants.playerHand);
+            $('#playerTotal').text(playerTotal)
+            
+            //======add card div to dealer facedown
+
+            BlackJackConstants.$dealerArea.append($('<div class="card down">'));
+            dealerTotal = BlackJackMethods.sum(BlackJackConstants.dealerHand);
+
+            //=====add card div to player
+
+            CardMethods.dealCard(BlackJackConstants.$playerArea, 'up', BlackJackConstants.playerHand);
+            playerTotal = BlackJackMethods.sum(BlackJackConstants.playerHand);
+            $('#playerTotal').text(playerTotal);
+            
+            //========add card div to deal
+
+            CardMethods.dealCard(BlackJackConstants.$dealerArea, 'up', BlackJackConstants.dealerHand);
+            dealerTotal = BlackJackMethods.sum(BlackJackConstants.dealerHand);
+            $('#dealerTotal').text(dealerTotal);
+            
+            // 
+
+            currentGameState = gameStates[0];
+            BlackJackConstants.$readOut.text(currentGameState);
+            if (playerTotal === 21) {
+                BlackJackConstants.$readOut.text('Jack Black! I mean Blackjack! Player wins!');
+                winner = 'player';
+                evaluateBet();
+                currentGameState = gameStates[2];
+            }
+        } else {
+            alert('The game is in progress. Please finish the hand');
+        }
+    }
 }
 
 class Player {
+    hit = () => {
+        if (currentGameState === gameStates[0]) {
+            
+            //========add card to player area
+            
+            CardMethods.dealCard(BlackJackConstants.$playerArea, 'up', BlackJackConstants.playerHand);
+            playerTotal = BlackJackMethods.sum(BlackJackConstants.playerHand);
+            $('#playerTotal').text(playerTotal);
+            BlackJackMethods.checkBust();
+        } else {
+            alert('It\'s not your turn.');
+        }
+    }
 
+    stand = () => {
+        if (currentGameState === gameStates[0]) {
+            
+            //=======set game state to dealer turn
+            
+            currentGameState = gameStates[1];
+            BlackJackConstants.$readOut.text(currentGameState);
+            $('.down').remove();
+            dealerTurn();
+        } else {
+            alert('Please deal a new hand.');
+        }
+    }
+
+    bet = () => {
+        currentBet = parseInt($('input').val());
+        if (currentBet <= playerChips) {
+            playerChips -= currentBet;
+            $chipsTotal.text(playerChips);
+            BlackJackConstants.$readOut.text(`You've bet ${currentBet} chips. Good luck!`);
+        } else if (currentBet > playerChips) {
+            alert('You don\'t have enough chips, bet again please.');
+        } else {
+            alert('You haven\'t bet yet, please enter a chip amount.')
+        }
+    }
 }
 
 class Table {
 
+    createDealer = () => {
+
+    }
+
+    createPlayers = (playerNum) => {
+
+    }
+    
+    checkWinner = () => {
+        if (currentGameState !== gameStates[2]) {
+            if (playerTotal > dealerTotal) {
+                BlackJackConstants.$readOut.text('You Win! Will you teach me your ways?');
+                winner = 'player';
+                evaluateBet();
+            } else if (dealerTotal > playerTotal) {
+                BlackJackConstants.$readOut.text('Dealer Wins. Crapola!');
+                winner = 'dealer';
+                evaluateBet();
+            } else {
+                BlackJackConstants.$readOut.text('You both have the same number, it\'s a push!')
+                winner = 'push';
+                evaluateBet();
+            }
+        }
+    }
+
+    checkBust = () => {
+        if (playerTotal > 21) {
+            currentGameState = gameStates[2]
+            BlackJackConstants.$readOut.text('Player Busted, Dealer Wins...I guess they don\'t know genius when they see it. ');
+            winner = 'dealer';
+            evaluateBet();
+        } else if (dealerTotal > 21) {
+            currentGameState = gameStates[2]
+            BlackJackConstants.$readOut.text('Dealer Busted! Player Wins! That\'s especially delicous.');
+            winner = 'player';
+            evaluateBet();
+        }
+    }
 }
 
 //=====================//
